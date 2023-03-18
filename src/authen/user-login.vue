5cdab4e94add878e40de92773b63bab3412d533c
oriental <1304206691@qq.com>
2023-03-18 15:32:21 +0800

    登录和注册页面提交

--- src/authen/user-login.vue
+++ src/authen/user-login.vue
@@ -1,10 +1,68 @@
 <template>
     <router-view>
-        你好
+        <div class="background">
+            <div class="form-login">
+                <el-form
+                    :model="login"
+                >
+                    <div style="text-align: center; margin-bottom: 18px;">
+                        <span style="color: white; font-size: 26px;">LuckCat登录</span>
+                    </div>
+                    <el-form-item prop="name">
+                        <el-input v-model="login.email" placeholder="邮箱"/>
+                    </el-form-item>
+                    <el-form-item prop="name">
+                        <el-input v-model="login.password" placeholder="密码" type="password" show-password/>
+                    </el-form-item>
+                    <el-form-item style="flex-wrap: wrap">
+                        <el-button type="primary" @click="sumbit" style="margin: 0 auto;">登录</el-button>
+                    </el-form-item>
+                    <div style="text-align: center;">
+                        <span style="color: white; font-size: 13px;">注册点击<router-link :to="{name:'register'}">这里</router-link></span>
+                    </div>
+                </el-form>
+            </div>
+        </div>
     </router-view>
 </template>
 
 <script setup>
+import { reactive } from 'vue';
+
+const login = reactive({
+    email: "",
+    password: ""
+})
+
+const sumbit = () => {
+
+}
 </script>
 
-<style></style>
+<style>
+.background{
+    background: url("@/public/no01.jpg");
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    background-size: 100% 100%;
+}
+.form-login{
+    position: absolute;
+    top: 45%;
+    left: 52%;
+    width: 400px;
+    height: 300px;
+    /* 设置毛玻璃效果 */
+    transform: translate(-50%,-50%);
+    background: rgba(255, 255, 255, 0.15);
+    -webkit-backdrop-filter: blur(8px);
+    backdrop-filter: blur(8px);
+    border-radius: 25px;
+    box-shadow:inset 0 0 6px rgba(255, 255, 255, 0.2);
+    /* 设置子div水平垂直居中 */
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+</style>
--- src/views/console/back-ground-view.vue
+++ src/views/console/back-ground-view.vue
@@ -1,12 +1,54 @@
 <template>
     <router-view>
-        nihao
+        <el-row :gutter="20" justify="space-between" style="margin: 0;">
+            <el-col :span="8">
+                <el-card class="box-card" shadow="never">
+                    <template #header>
+                        <div class="card-header">
+                            <span>图片总数</span>
+                        </div>
+                    </template>
+                    <div style="height: 60px; display: flex; align-items: center;">{{ state.total_photo }}</div>
+                </el-card>
+            </el-col>
+            <el-col :span="8">
+                <el-card class="box-card" shadow="never">
+                    <template #header>
+                        <div class="card-header">
+                            <span>用户总数</span>
+                        </div>
+                    </template>
+                    <div style="height: 60px; display: flex; align-items: center;">{{ state.total_user }}</div>
+                </el-card>
+            </el-col>
+            <el-col :span="8">
+                <el-card class="box-card" shadow="never">
+                    <template #header>
+                        <div class="card-header">
+                            <span>剩余容量</span>
+                        </div>
+                    </template>
+                    <div style="height: 60px; display: flex; align-items: center;">{{ state.total_space }}</div>
+                </el-card>
+            </el-col>
+        </el-row>
     </router-view>
 </template>
 
 <script setup>
+import { reactive } from "vue";
+
+const state = reactive({
+    total_user : "total_user",
+    total_photo: "total_photo",
+    total_space: "total_space",
+    personal_photo: "personal_photo",
+    personal_space: "personal_space",
+})
 </script>
 
 <style>
-
+.el-card{
+    height: 160px;
+}
 </style>
